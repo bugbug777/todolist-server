c3e386224c10c24a594177a904c2d1889d275775
sihle <gubug777@gmail.com>
2022-07-25 12:33:27 +0800

    [fix] 修正 CORS 語法錯誤

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors);
+app.use(cors());
 
 app.use('/users', usersRouter /* #swagger.tags = ['User - 使用者'] */);
 app.use('/todos', todosRouter /* #swagger.tags = ['Todo - 待辦事項'] */)
